6e335c7878bd45e59fd3c79815b9c6f6b5d043bb
Steven Sawadisavi <stevesavi+lewagon@gmail.com>
2026-02-24 15:17:11 +0900

    began working on view for user_applications

--- app/controllers/user_applications_controller.rb
+++ app/controllers/user_applications_controller.rb
@@ -2,4 +2,14 @@ class UserApplicationsController < ApplicationController
   def new
     @user_application = UserApplication.new
   end
+
+  def create
+    @user_application = UserApplication.new(user_application_params)
+  end
+
+  private
+
+  def user_application_params
+    params.require(:user_application).permit(:title)
+  end
 end
