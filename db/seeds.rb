488cd991ecceffd0c9511814e476e3d3d7d4b7dd
Jonathan Kiichi Kimura <picard33470@gmail.com>
2026-02-24 17:04:03 +0900

    Altered seeds.rb

--- db/seeds.rb
+++ db/seeds.rb
@@ -17,8 +17,8 @@ puts "Seeding DB"
   user = User.new(
     first_name: Faker::Name.first_name,
     last_name: Faker::Name.last_name,
-    email: Faker::Internet.email,
-    password: Faker::Internet.password
+    email: "email#{User.count}@domain.com",
+    password: "123456"
   )
   user.save!
   application_journey = ApplicationJourney.new(
