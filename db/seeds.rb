3a12c4664efd2c1c582a95dfb0d09c4f661ab8f8
Steven Sawadisavi <stevesavi+lewagon@gmail.com>
2026-02-26 14:58:54 +0900

    commented out user application, chat, and message generation

--- db/seeds.rb
+++ db/seeds.rb
@@ -17,14 +17,17 @@ ApplicationJourney.destroy_all
 9.times do |i|
   application_journey = ApplicationJourney.new(
     application_road: i + 1,
-    system_prompt: "DWJANAWIOSBIASDA"
   )
+  application_journey.system_prompt = ApplicationJourney::DESCRIPTION[application_journey.application_road.to_sym]
   application_journey.description = ApplicationJourney::DESCRIPTION[application_journey.application_road.to_sym]
   application_journey.save!
 end
 puts "... #{ApplicationJourney.count} application journeys added"
 
-puts "Seeding test users, chats, and messages"
+puts "Seeding test users"
+# puts "Seeding test chats"
+# puts "Seeding test messages"
+
 9.times do |i|
   user = User.new(
     first_name: Faker::Name.first_name,
@@ -33,28 +36,28 @@ puts "Seeding test users, chats, and messages"
     password: "123456"
   )
   user.save!
-  user_application = UserApplication.new(
-    user: user,
-    application_journey: ApplicationJourney.all[i],
-    title: Faker::Artist.name
-  )
-  user_application.save!
-  chat = Chat.new(
-    title: Faker::Military.navy_rank,
-    user_application: user_application
-  )
-  chat.save!
-  3.times do
-    message = Message.new(
-      chat: chat,
-      content: Faker::Lorem.paragraph,
-      role: ["assistant", "user"].sample
-    )
-    message.save!
-  end
+  # user_application = UserApplication.new(
+  #   user: user,
+  #   application_journey: ApplicationJourney.all[i],
+  #   title: Faker::Artist.name
+  # )
+  # user_application.save!
+  # chat = Chat.new(
+  #   title: Faker::Military.navy_rank,
+  #   user_application: user_application
+  # )
+  # chat.save!
+  # 3.times do
+  #   message = Message.new(
+  #     chat: chat,
+  #     content: Faker::Lorem.paragraph,
+  #     role: ["assistant", "user"].sample
+  #   )
+  #   message.save!
+  # end
 end
 
 puts "Finished putting #{User.count} user(s)"
-puts "Finished putting #{UserApplication.count} user application(s)"
-puts "Finished putting #{Chat.count} chat(s)"
-puts "Finished putting #{Message.count} message(s)"
+# puts "Finished putting #{UserApplication.count} user application(s)"
+# puts "Finished putting #{Chat.count} chat(s)"
+# puts "Finished putting #{Message.count} message(s)"
