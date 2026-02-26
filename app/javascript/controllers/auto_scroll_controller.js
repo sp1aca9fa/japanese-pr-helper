32691276f66ef4c1cc35f76274f3c12da64ea009
Steven V. Sawadisavi <stevesavi@gmail.com>
2026-02-26 17:30:24 +0900

    Revert "auto scroll implemented"

--- app/javascript/controllers/auto_scroll_controller.js
+++
@@ -1,9 +0,0 @@
-import { Controller } from "@hotwired/stimulus"
-
-// Connects to data-controller="auto-scroll"
-export default class extends Controller {
-  connect() {
-    // console.log("hi from auto-scroll controller");
-    this.element.scrollIntoView();
-  }
-}
